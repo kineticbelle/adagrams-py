5d9ab89a8fd94c82b6912ca28f50da48755b7366
Natasha Gaye <natashagaye11@gmail.com>
2025-03-20 17:20:34 -0500

    third wave has passed through tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -73,7 +73,26 @@ def uses_available_letters(word, letter_bank):
     return True 
 
 def score_word(word):
-    pass
+    score_word_points = {
+        'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+        'D': 2, 'G': 2,
+        'B': 3, 'C': 3, 'M': 3, 'P': 3,
+        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5,
+        'J': 8, 'X': 8,
+        'Q': 10, 'Z': 10
+}
+
+    total_score = 0
+    for letter in word:
+        upper_letter = letter.upper()
+        if upper_letter in score_word_points:
+            total_score += score_word_points[upper_letter]
+        
+    if 7 <= len(word) <= 10:
+        total_score += 8
+    
+    return total_score        
 
 def get_highest_word_score(word_list):
     pass
